7d196226cad17f2f6557a496c198c111009d67ff
MrShibolet <MrShibolet@users.noreply.github.com>
2020-03-30 12:32:04 +0000

    automated commit by submit50 [lang=C.UTF-8]

--- mario.c
+++ mario.c
@@ -5,24 +5,29 @@
 int main(void)
 {
     int hight = 0;
-     while(hight <= 0 || hight >= 9){
-     hight = get_int("Please enter hight\n");
-     }
-     /* row */
-  for(int i=hight;i!=0;i-=1){
-      /*char*/
-    for(int j=hight-i+2;j<=hight;j+=1){
-        printf(" ");
-    }
-    for(int f=i;f<=hight;f+=1)
+    /*main check to see correct input*/
+    while (hight <= 0 || hight >= 9)
     {
-        printf("#");
+        hight = get_int("Please enter hight\n");
+    }
+    /* row */
+    for (int i = hight; i != 0; i -= 1)
+    {
+        /*char*/
+        for (int j = hight - i + 2; j <= hight; j += 1)
+        {
+            printf(" ");
         }
-    printf("  ");
-    for(int z=i;z<=hight;z+=1)
-    {
-        printf("#");
+        for (int f = i; f <= hight; f += 1)
+        {
+            printf("#");
+        }
+        /*gap in middle*/
+        printf("  ");
+        for (int z = i; z <= hight; z += 1)
+        {
+            printf("#");
+        }
+        printf("\n");
     }
-    printf("\n");
-  }
 }
