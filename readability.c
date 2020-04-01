037f925f5d218cff193698af70c19191ee29951e
MrShibolet <MrShibolet@users.noreply.github.com>
2020-04-01 15:13:29 +0000

    automated commit by submit50

--- readability.c
+++ readability.c
@@ -6,49 +6,59 @@
 
 int main(void)
 {
-    int scount=0;
-    int wcount=0;
-    int lcount=0;
+    int scount = 0;
+    int wcount = 0;
+    int lcount = 0;
     string s = get_string("Text:");
 // sentence counter
-    for (int i = 0, n = strlen(s); i < n; i++) {
-        if ((s[i] == '.') || (s[i] == '?') || (s[i] == '!')) {
-            scount=scount+1;
-            if(s[i+1] != ' ') {
-                wcount=wcount+1;
+    for (int i = 0, n = strlen(s); i < n; i++) 
+    {
+        if ((s[i] == '.') || (s[i] == '?') || (s[i] == '!')) 
+        {
+            scount = scount + 1;
+            if (s[i + 1] != ' ') 
+            {
+                wcount = wcount + 1;
             }
         }
     }
 // letter counter
-    for (int i = 0, n = strlen(s); i < n; i++) {
-        if (isalpha(s[i])) {
-            lcount=lcount+1;
+    for (int i = 0, n = strlen(s); i < n; i++) 
+    {
+        if (isalpha(s[i]))
+        {
+            lcount = lcount + 1;
         }
     }
 // word counter
-    for (int i = 0, n = strlen(s); i < n; i++) {
-        if ((s[i] == ' ') && (s[i+1] != ' ')) {
-            wcount=wcount+1;
+    for (int i = 0, n = strlen(s); i < n; i++) 
+    {
+        if ((s[i] == ' ') && (s[i + 1] != ' ')) 
+        {
+            wcount = wcount + 1;
         }
     }
 //    printf("scount %d\n",scount);
 //    printf("lcount %d\n",lcount);
 //    printf("wcount %d\n",wcount);
 
-    float L = (100.0/wcount) * lcount;
-    float S = (100.0/wcount) * scount;
+    float L = (100.0 / wcount) * lcount;
+    float S = (100.0 / wcount) * scount;
 //    printf("L %f\n",L);
 //    printf("S %f\n",S);
     float ind = 0.0588 * L - 0.296 * S - 15.8;
     int index = round(ind);
 //    printf("Index %f",ind);
-    if(index < 1) {
+    if (index < 1) 
+    {
         printf("Before Grade 1\n");
     }
-    else if(index >= 16) {
-            printf("Grade 16+\n");
+    else if (index >= 16) 
+    {
+        printf("Grade 16+\n");
     }
-    else{
-        printf("Grade %d\n",index);
+    else
+    {
+        printf("Grade %d\n", index);
     }
 }
