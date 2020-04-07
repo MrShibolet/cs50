5af995814088336787fbab09b5354fd1576346b0
MrShibolet <MrShibolet@users.noreply.github.com>
2020-04-07 12:04:08 +0000

    automated commit by check50 [check50=True]

--- runoff.c
+++ runoff.c
@@ -185,7 +185,7 @@ int find_min(void)
 int tester = voter_count;
      for (int i = 0; i < candidate_count; i++)
     {
-        if ((tester < candidates[i].votes) && (candidates[i].eliminated == false))
+        if ((tester > candidates[i].votes) && (candidates[i].eliminated == false))
         {
             tester = candidates[i].votes;
         }
