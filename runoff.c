5b316aba33003f637801ff0aaf0a76bdf8605bc7
MrShibolet <MrShibolet@users.noreply.github.com>
2020-04-07 11:55:00 +0000

    automated commit by check50 [check50=True]

--- runoff.c
+++ runoff.c
@@ -169,7 +169,7 @@ bool print_winner(void)
 {
      for (int i = 0; i < candidate_count; i++)
     {
-        if (candidates[i].votes >= voter_count/2 )
+        if (candidates[i].votes > ceil(voter_count/2.0) )
         {
             printf("%s\n",candidates[i].name);
             return true;
