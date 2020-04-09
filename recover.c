5baea16f88f94665f00b676057ae2ee745e5f11c
MrShibolet <MrShibolet@users.noreply.github.com>
2020-04-09 19:36:15 +0000

    automated commit by check50 [check50=True]

---
+++ recover.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+typedef uint8_t BYTE;
+
+int main(int argc, char *argv[])
+{
+int i = 0;
+FILE *img = NULL;
+int neeg = 0;
+char filename[7];
+unsigned char bytes[4];
+  if (argc != 2)
+    {
+        printf("Usage: ./recover image\n");
+        return 1;
+    }
+    FILE *file = fopen(argv[1], "r");
+    if (!file)
+    {
+        return 1;
+    }
+    int length = 0;
+    while(fgetc(file) != EOF)
+    {
+        length += 1;
+    }
+    int blocks = length / 512;
+    file = fopen(argv[1], "r");
+    for (int j=0; j < blocks; j += 1)
+    {
+        BYTE now[512];
+        fread(now, 1, 512, file);
+        if (now[0] == 0xff && now[1] == 0xd8 && now[2] == 0xff && ((now[3] & 0xf0) == 0xe0))
+        {
+            if(i != 0)
+            {
+                fclose(img);
+            }
+            sprintf(filename, "%03i.jpg",i);
+            img = fopen(filename, "w"); 
+            fwrite(now, 1, 512, img);
+            i += 1;
+        }
+    }
+    fclose(file);
+    fclose(img);
+}
